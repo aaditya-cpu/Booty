24a662176043e419064dd8d09ce28b3fec9e8d90
Uzumaki <aaditya.goenka5@gmail.com>
2022-05-06 15:36:37 +0530

    updated office

--- boot/J/index.js
+++ boot/J/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
     var co = 0
 
+    $("#lolImg").hide();
+    $("#lelImg").hide();
 
     $("#lol").click(function() {
         if (co % 2 == 0) {
-            $("#lolImg").hide();
-            $("#lelImg").show();
+            $("#lolImg").hide(1500);
+            $("#lelImg").show(1500);
             co = co + 1;
         } else {
 
-            $("#lolImg").show();
-            $("#lelImg").hide();
+            $("#lolImg").show(1500);
+            $("#lelImg").hide(1500);
             co = co + 1;
         }
     });
