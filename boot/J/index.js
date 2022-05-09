3aa6b8df89c14acdb5ded63ee1a8cacc9edbc393
Uzumaki <aaditya.goenka5@gmail.com>
2022-05-09 09:51:23 +0530

    updates

--- boot/J/index.js
+++ boot/J/index.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
     var co = 0
 
+    // $("#lolImg").hide();
+    // $("#lelImg").hide();
     $("#lolImg").hide();
     $("#lelImg").hide();
 
     $("#lol").click(function() {
         if (co % 2 == 0) {
-            $("#lolImg").hide(1500);
-            $("#lelImg").show(1500);
+            $("#lolImg").fadeToggle(1500);
+
+            // $("#lolImg").hide(1500);
+            // $("#lelImg").show(1500);
             co = co + 1;
         } else {
 
-            $("#lolImg").show(1500);
-            $("#lelImg").hide(1500);
+            $("#lelImg").fadeToggle(1500);
+            // $("#lolImg").show(1500);
+            // $("#lelImg").hide(1500);
             co = co + 1;
         }
     });
